c303ac7504ffdb9237b87feb2ba8f072d18efc72
oleg pupkin <olegpupikin@gmail.com>
2023-09-08 12:00:17 +0300

    add number 2

---
+++ src/test/java/homework_nr_2/HomeWork2.java
@@ -0,0 +1,44 @@
+package homework_nr_2;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class HomeWork2 {
+    public static void main(String[]args) {
+
+
+    //Just print - Hello World!
+    System.out.println("Hello World!");
+
+    //Announcement variables
+    long CurrentWorldPopulation = 7811080815L;
+    int CarsProducedThisYear = 55235358;
+    char FirsLetterInAlphabet = 'A';
+    boolean JavalsAmazing = true;
+    float Pi = 3.14159F;
+
+    // Print out all variables
+        System.out.println ("Current world population is = " +CurrentWorldPopulation+ "\n" + "Cars produced this year is = " +CarsProducedThisYear+ "\n" +FirsLetterInAlphabet+ "\n"
+                +JavalsAmazing+ "\n" +Pi
+        );
+      //
+      double Price, Tax, Sum, Total ;
+              Price = 5.893790478390475390D;
+              Tax = 2.0394583058035834456456905D;
+              Sum = 0;
+      int quantity = 3456;
+        //
+        System.out.println("Price = " +Price+ "\n" +"Tax = " +Tax );
+
+       // Sum of prices, taxes and cars quantity
+        Total = Price + (Tax * quantity) + Sum;
+
+        System.out.println("Total = " +Total);
+
+    }
+
+
+
+
+
+}
