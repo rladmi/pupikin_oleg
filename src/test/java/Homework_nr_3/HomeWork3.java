46dd2418eeb3f68a5369bbbf702cb158a41c45f4
oleg pupkin <olegpupikin@gmail.com>
2023-09-12 09:27:03 +0300

    homework 11-09

--- src/test/java/Homework_nr_3/HomeWork3.java
+++ src/test/java/Homework_nr_3/HomeWork3.java
@@ -2,7 +2,7 @@ package Homework_nr_3;
 
 public class HomeWork3 {
 
-    public static void main(String[]args) {
+    public static void main(String[] args) {
 
         // Show month
         int month = 2;
@@ -54,7 +54,8 @@ public class HomeWork3 {
             case 12:
                 System.out.println("December");
                 break;
-            default: System.out.println("Invalid month day");
+            default:
+                System.out.println("Invalid month day");
         }
 
         for (float i = 100; i <= 1000; i++) {
@@ -63,5 +64,67 @@ public class HomeWork3 {
                 i++;
             }
         }
+
+
+
+/*
+ int i = 1;
+ int  j = 3;
+        while (i < 97) {
+            if (i % 2 != 0 && i % 1 == 0)
+       // while (j < 99) {
+         //   if (j % 2 != 0 && j % 1 == 0)
+            i++;
+            System.out.println(i);
         }
-}
+         //  i++;
+        }
+*/
+        // float c = 0;
+        // float d = 0;
+
+       // for (int k = 0; k <= 0; k++) {
+            for (float i = 1; i <= 97; i+=2){
+                System.out.println(i/(i+2));
+            }
+
+                //if (i % 2 != 0 && i % 1 == 0)
+            //System.out.println(i);
+
+                 /*  for (int b = 0; b <= 0; b++)
+                        for (float j = 3; j <= 99; j++)
+                            if (j % 2 != 0 && j % 1 == 0)
+                                System.out.println(j);*/
+
+                        //  j++;
+
+/*int i=1;
+int j=3;
+        for (int k = 0; k < 1; k++) {
+            while (i <= 97 && j <= 99 && j % 2 != 0 && j % 1 == 0 && i % 2 != 0 && i % 1 == 0 ) {
+                System.out.println(i);
+                i++;*/
+
+
+           /* for (int k = 0; k <= 0; k++) {
+                for (float i = 1; i <= 97; i++) {
+                    for (float j = 3; j <= 99; j++)
+                    if (j % 2 != 0 && j % 1 == 0 && i % 2 != 0 && i % 1 == 0)
+                    System.out.println(i);*/
+                }
+
+            }
+        //}
+
+
+
+
+
+
+
+
+
+
+
+
+
